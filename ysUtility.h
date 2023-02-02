8dcc5525edbcd0d3f050dfb4d6289833a694079b
최윤서 <chldbstj4536@gmail.com>
2023-02-02 16:18:25 +0900

    PassKey 패턴 구현

--- ysUtility.h
+++ ysUtility.h
@@ -61,4 +61,51 @@ public:\
     template<typename... _Args>\
     enable_make_shared(_Args&&... args) : _CLASS(std::forward<_Args>(args)...) { }\
 };
+
+/**
+ * @brief PassKey 패턴에서 어떤 클래스를 통과시킬지 결정하는 클래스
+ * 
+ * PassKey 패턴을 템플릿을 사용하여 구현
+ * 
+ * 사용하고자 하는 멤버함수에서 PassKey<_Certs...> 클래스를 파라미터로 두어
+ * 가변인자(_Certs...)에 해당하는 클래스만 호출할 수 있도록 구현.
+ * 
+ * @example
+ * 허용하고자 하는 클래스들을 가변 인자 템플릿에 넣어준다.
+ * Ret SomeSecureMemFn(PassKey<Foo, Bar, ...>, ...);
+ * 
+ * @tparam _Certs 호출 가능한 클래스들
+ */
+template<class... _Certs>
+class PassKey
+{
+public:
+    template <class _Cert>
+    requires (same_as<_Certs, _Cert> || ...)
+    PassKey(PassKey<_Cert> const &) {}
+};
+/**
+ * @brief PassKey 패턴에서 자신을 인증하는데 사용되는 CRTP 클래스
+ * 
+ * PassKey 패턴을 템플릿을 사용하여 구현
+ * 
+ * PassKey<Self>를 상속받아 인증을 요구하는(PassKey<_Certs...>를 입력받는) 함수에 인증하는데 사용
+ * PassKey<_Cert>를 상속받고 자기 자신을 매개변수로 넣어서 사용
+ * 
+ * @example
+ * 클래스 상속 후
+ * class Foo : public PassKey<Foo> {}
+ * 호출하고자 하는 함수의 인증 매개변수에 자기 자신을 대입
+ * SomeSecureMemFn(*this, ...);
+ * 
+ * @tparam _Cert 인증하고자 하는 클래스 자신
+ */
+template <class _Cert>
+class PassKey<_Cert>
+{
+private:
+    PassKey() = default;
+
+    friend _Cert;
+};
 _YS_END
